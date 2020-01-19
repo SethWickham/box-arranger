d3b3416b6a7c15bb3b9da3b41e99b67b39e5788a
SethWickham <seth.wickham@hotmail.com>
2020-01-19 10:50:25 -0800

    created button added basic styling

---
+++ react-box-arranger/src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from '../Button/Button.module.css';
+
+const button = () => (
+  <div>
+    <button className={styles.Button}>Button</button>
+  </div>
+);
+
+export default button;
--- react-box-arranger/src/components/Display/Display.js
+++ react-box-arranger/src/components/Display/Display.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 
 import styles from '../Display/Display.module.css';
 import Box from '../../components/Box/Box';
+import Button from '../../components/Button/Button';
 
 class display extends Component {
   render() {
     return (
-      <div className={styles.Display}>
-        Hi
-        <div>
+      <div className={styles.Container}>
+        <div className={styles.Display}>
           {' '}
           <Box></Box>
+          <Button></Button>
         </div>
       </div>
     );
